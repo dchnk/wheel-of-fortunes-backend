f34e56f57239299552378d410871a23d67265078
dchnk <seroxe2@yandex.ru>
2023-11-06 18:20:31 +0700

    fix

--- database/database.js
+++ database/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 module.exports.sequelize = new Sequelize('FORTUNE', 'root', 'D9LIEy3MMuXbVcohBJ.!', {
-  host: 'localhost',
+  host: '127.0.0.1',
   dialect: 'mysql',
   logging: false
 })
